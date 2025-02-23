4a4ad90df3c4fa57a03b762a0d5774fee8153be0
GbArivazhagan <argb@kth.se>
2025-02-23 21:47:28 +0100

    Add: codes

---
+++ 01_codes/02_E-predictions/config_deep.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mon  1 10:06:34 2021
+
+@author: ari
+"""
+
+class NN_WallRecon:
+    # Problem definition
+    N_VARS_IN = 3
+    N_VARS_OUT = 1
+
+    VARS_NAME_IN = ('v','w','u','p')
+    VARS_NAME_OUT = ('5','1')
+    
+    NORMALIZE_INPUT = True        # Normalize input to have zero average and unit variance
+    SCALE_OUTPUT = False           # Scale output with the ratio of the RMS values
+    
+    FLUCTUATIONS_PRED = True      # Predict only the fluctuations of the output
+    RELU_THRESHOLD = -50.0           # Relu threshold for last layer in main network
+
+    TRAIN_YP = 15
+    TARGET_YP = (15)
+    
+    # Hardware definition
+    ON_GPU = True
+    N_GPU = 4
+    WHICH_GPU_TRAIN = 0
+    WHICH_GPU_TEST = 0
+
+    # Dataset definition
+    CUR_PATH = '/mimer/NOBACKUP/groups/kthmech/argb/02_VE/n25/04_shear_yp15'
+    ## Ret180
+    DS_PATH_TRAIN = '/mimer/NOBACKUP/groups/kthmech/argb/02_VE/n25/99_dataset/Train'
+    DS_PATH_VALIDATION = '/mimer/NOBACKUP/groups/kthmech/argb/02_VE/n25/99_dataset/Validation'
+    DS_PATH_TEST = '/mimer/NOBACKUP/groups/kthmech/argb/02_VE/n25/99_dataset/Test'
+
+    N_SAMPLES_TRAIN = (2000,2000,2000,2000,2000,712,2000,2000,2000,2000,2000,712)
+    N_SAMPLES_VALIDATION = (2000,1210,2000,1210)
+    
+    INTERV_TRAIN = 1
+
+    N_SAMPLES_TEST = (2000,1210)
+
+    INTERV_TEST = 1
+    
+    TIMESTAMP = ''  # add some information here to recognize the model
+    
+    FROM_CKPT = True     # to load the model saved at checkpoint CKPT
+    CKPT = 45
+    
+    # Prediction that has to be post-processed
+    PRED_NB = 0
+    
+    DATA_AUG = False
+    
+    # Network definition
+    NET_MODEL = 1
+    # See README for network descriptions
+    
+    # Model-specific options (syntax: [option]_[net_model])
+    PAD_1 = 'wrap'
+    
+    # Training options
+    INIT = 'random' # default value: 'random'
+    INIT_MODEL = ''
+    TRANSFER_LEARNING = False
+    N_TRAINABLE_LAYERS = 3
+    
+    N_EPOCHS = 50
+    BATCH_SIZE = 4
+    VAL_SPLIT = 0.2
+    
+    INIT_LR = 0.001
+    LR_DROP = 0.5
+    LR_EPDROP = 20.0#12.0
+    
+    # Callbacks specifications
+    TB_HIST_FREQ = 0
+    CKPT_FREQ = 10
